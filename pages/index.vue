ea327208d55cf6f4fa02dc4ae5d008e750388a45
Null <null@voidnull.dev>
2021-03-13 22:42:55 -0600

    rm more filler

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
         Oh, you want to know about me?
       </h1>
       <p class="text-white max-w-screen-sm md:max-w-screen-md mt-4 text-md lg:text-xl">
-        This’ll be, interesting. Hope you like it.
+        Ok then.<br>
         I see myself as a gamer, a person, and a developer. In that order.
         I primarily like to play online games, but will play single player games when in the mood. I like a lot of fast paced games, like shooters, as well as survival type games (although I prefer those to be online). Open world games are generally the most fun. I don’t really fit into a gaming profile. So lets move on to developer.
         I am by no means certified in any way, however that doesn’t mean I do not enjoy this, at times. I enjoy Pythons syntax and can usually agree with TypeScript but Java is just a pain no matter what I do, therefore I do not use Java, and I prefer not to. I have yet to really use Python. I do most of my development on Github, and primarily use either TypeScript or JavaScript.

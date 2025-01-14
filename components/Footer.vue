3463928ed024ceeb74cd404cb981748956271a72
Aervyon <aervy@aervyon.com>
2025-01-13 20:21:42 -0600

    fix(components/Footer.vue): Use sematic footer for better accessibility

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
-	<div
+	<footer
 		class="flex text-accent-secondary-dark p-4 justify-between text-xs md:text-sm mt-10"
 	>
 		<p>Copyright (C) 2020-{{ new Date().getFullYear() }} Aervyon</p>
 		<div>All rights reserved</div>
-	</div>
+	</footer>
 </template>
